ed3a01e7f56544ff346d76da4d45789221226f1d
Chathura Jayasanka <cjayasanka93@gmail.com>
2017-12-17 15:44:58 +0530

    login working9

--- app/src/main/java/archfx/com/epidermicare/app/AppConfig.java
+++ app/src/main/java/archfx/com/epidermicare/app/AppConfig.java
@@ -6,9 +6,9 @@ package archfx.com.epidermicare.app;
 
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.0.102/android_login_api/login.php";
+    public static String URL_LOGIN = "https://cjayasanka93.000webhostapp.com/android_login_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.0.102/android_login_api/register.php";
+    public static String URL_REGISTER = "https://cjayasanka93.000webhostapp.com/android_login_api/register.php";
 
 }
