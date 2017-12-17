9d549b43f265de096a72fc901ab2c14a09592ca8
archfx <cybx@live.com>
2017-12-17 18:21:48 +0530

    login layout

--- app/src/main/java/archfx/com/epidermicare/activity/MainActivity.java
+++ app/src/main/java/archfx/com/epidermicare/activity/MainActivity.java
@@ -1,111 +1,31 @@
 package archfx.com.epidermicare.activity;
 
-
-//import android.annotation.SuppressLint;
-//import android.content.Intent;
-//import android.support.v7.app.ActionBar;
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.os.Handler;
-//import android.view.MotionEvent;
-//import android.view.View;
-//import android.view.Window;
-//import android.widget.ImageButton;
-//import android.widget.TextView;
-//
-//import archfx.com.epidermicare.R;
-//import archfx.com.epidermicare.app.User;
-//
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//
-//    ImageButton confirmbtn;
-//    TextView confirmbuttonText;
-//    User currentUsr;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//        setContentView(R.layout.activity_main);
-//        ActionBar actionBar = getSupportActionBar();
-//        actionBar.hide();
-//
-//        confirmbtn=(ImageButton)findViewById(R.id.confirmButton);
-//        confirmbuttonText=(TextView)findViewById(R.id.confirmText);
-//
-//        //if user is a doctor set confirmation of epidemi to visible
-//        if(currentUsr.isIsdoctor())
-//        {
-//            confirmbtn.setVisibility(View.VISIBLE);
-//            confirmbuttonText.setVisibility(View.VISIBLE);
-//        }
-//        //if a normal user set this to gone
-//        else
-//        {
-//            confirmbtn.setVisibility(View.GONE);
-//            confirmbuttonText.setVisibility(View.GONE);
-//        }
-//
-//
-//
-//        //setTitle("MainMenu");
-//
-//    }
-//
-//    public  void onClickProfile(View view)
-//    {
-//        if(view.getId()==R.id.ProfileButton) {
-//            Intent b = new Intent(this, ProfileActivity.class);
-//            startActivity(b);
-//        }
-//
-//    }
-//    public void onClickReport(View view)
-//    {
-//        if(view.getId()==R.id.ReportButton) {
-//            Intent a = new Intent(this, ReportActivity.class);
-//            startActivity(a);
-//        }
-//
-//    }
-//    public void onClickStatus(View view)
-//    {
-//        if(view.getId()==R.id.StatusButton) {
-//            Intent c = new Intent(this, StatusActivity.class);
-//            startActivity(c);
-//        }
-//
-//    }
-//    public void onClickConfirm(View view)
-//    {
-//        if(view.getId()==R.id.confirmButton) {
-//            Intent d = new Intent(this,ConfirmActivity.class);
-//            startActivity(d);
-//        }
-//
-//    }
-//}
-
 import java.util.HashMap;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+
 import archfx.com.epidermicare.R;
+import archfx.com.epidermicare.app.User;
 import archfx.com.epidermicare.helper.SQLiteHandler;
 import archfx.com.epidermicare.helper.SessionManager;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private TextView txtName;
     private TextView txtEmail;
-    private Button btnLogout;
+  //  private Button btnLogout;
+    ImageButton confirmbtn;
+    TextView confirmbuttonText;
+    User currentUsr=new User();
 
     private SQLiteHandler db;
     private SessionManager session;
@@ -115,9 +35,29 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtName = (TextView) findViewById(R.id.name);
-        txtEmail = (TextView) findViewById(R.id.email);
-        btnLogout = (Button) findViewById(R.id.btnLogout);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        //txtName = (TextView) findViewById(R.id.name);
+        //txtEmail = (TextView) findViewById(R.id.email);
+        //   btnLogout = (Button) findViewById(R.id.btnLogout);
+        confirmbtn = (ImageButton) findViewById(R.id.confirmButton);
+        confirmbuttonText = (TextView) findViewById(R.id.confirmText);
+
+
+
+        //if user is a doctor set confirmation of epidemi to visible
+/*
+        if (currentUsr.isIsdoctor()) {
+            confirmbtn.setVisibility(View.VISIBLE);
+            confirmbuttonText.setVisibility(View.VISIBLE);
+        }
+        //if a normal user set this to gone
+        else {
+            confirmbtn.setVisibility(View.GONE);
+            confirmbuttonText.setVisibility(View.GONE);
+        }
+*/
 
         // SqLite database handler
         db = new SQLiteHandler(getApplicationContext());
@@ -136,17 +76,18 @@ public class MainActivity extends Activity {
         String email = user.get("email");
 
         // Displaying the user details on the screen
-        txtName.setText(name);
-        txtEmail.setText(email);
+       // txtName.setText(name);
+      //  txtEmail.setText(email);
 
         // Logout button click event
-        btnLogout.setOnClickListener(new View.OnClickListener() {
+/*        btnLogout.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 logoutUser();
             }
         });
+    }*/
     }
 
     /**
@@ -162,5 +103,38 @@ public class MainActivity extends Activity {
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
         finish();
+    }
+
+        public  void onClickProfile(View view)
+    {
+        if(view.getId()==R.id.ProfileButton) {
+            Intent b = new Intent(this, ProfileActivity.class);
+            startActivity(b);
+        }
+
+    }
+    public void onClickReport(View view)
+    {
+        if(view.getId()==R.id.ReportButton) {
+            Intent a = new Intent(this, ReportActivity.class);
+            startActivity(a);
+        }
+
+    }
+    public void onClickStatus(View view)
+    {
+        if(view.getId()==R.id.StatusButton) {
+            Intent c = new Intent(this, StatusActivity.class);
+            startActivity(c);
+        }
+
+    }
+    public void onClickConfirm(View view)
+    {
+        if(view.getId()==R.id.confirmButton) {
+            Intent d = new Intent(this,ConfirmActivity.class);
+            startActivity(d);
+        }
+
     }
 }
