3a137dcb3151edeb7b600182dc7ddfb47c6e3a2d
Chathura Jayasanka <cjayasanka93@gmail.com>
2017-12-17 13:21:26 +0530

    login working2

--- app/src/main/java/archfx/com/epidermicare/activity/SignUpActivity.java
+++ app/src/main/java/archfx/com/epidermicare/activity/SignUpActivity.java
@@ -1,12 +1,8 @@
 package archfx.com.epidermicare.activity;
 
-import android.annotation.SuppressLint;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
 
 import archfx.com.epidermicare.R;
 
@@ -20,7 +16,7 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_register);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
