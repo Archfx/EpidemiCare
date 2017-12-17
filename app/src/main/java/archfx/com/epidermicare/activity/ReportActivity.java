fbf6d40905588e01cff7e589388370a2d52e8897
Chathura Jayasanka <cjayasanka93@gmail.com>
2017-12-17 21:19:00 +0530

    login working11

--- app/src/main/java/archfx/com/epidermicare/activity/ReportActivity.java
+++ app/src/main/java/archfx/com/epidermicare/activity/ReportActivity.java
@@ -1,20 +1,49 @@
 package archfx.com.epidermicare.activity;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import archfx.com.epidermicare.R;
+import archfx.com.epidermicare.app.AppConfig;
+import archfx.com.epidermicare.app.AppController;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class ReportActivity extends AppCompatActivity {
+
+
+    private Button btnReport;
+    private EditText pName;
+    private EditText dName;
+    private EditText area;
+    private EditText pNIC;
+    private EditText details;
+    private EditText date;
+    private ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +53,126 @@ public class ReportActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
+        pName = (EditText) findViewById(R.id.pName);
+        pNIC = (EditText) findViewById(R.id.nicPatient);
+        dName = (EditText) findViewById(R.id.dName);
+        area = (EditText) findViewById(R.id.area);
+        details = (EditText) findViewById(R.id.details_epi);
+        date = (EditText) findViewById(R.id.date);
+        btnReport = (Button) findViewById(R.id.btnReport);
 
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+
+        // Register Button Click event
+        btnReport.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String patient = pName.getText().toString().trim();
+                String nic = pNIC.getText().toString().trim();
+                String disease = dName.getText().toString().trim();
+                String district = area.getText().toString().trim();
+                String detail = details.getText().toString().trim();
+                String dateofincident = date.getText().toString().trim();
+
+                if (!patient.isEmpty() && !disease.isEmpty() && !nic.isEmpty() && !district.isEmpty() && !dateofincident.isEmpty()) {
+                    reportDisease(patient, nic, disease, district, detail, dateofincident);
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Please enter details!", Toast.LENGTH_LONG)
+                            .show();
+                }
+            }
+        });
+
+
+    }
+
+    private void reportDisease(final String patient, final String ,
+                              final String password,final String dateOfBirth, final String gender,
+                              final String district,final String guardiansNic,final String nic) {
+        // Tag used to cancel the request
+        String tag_string_req = "req_register";
+
+        pDialog.setMessage("Registering ...");
+        showDialog();
+
+        StringRequest strReq = new StringRequest(Request.Method.POST,
+                AppConfig.URL_REGISTER, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Register Response: " + response.toString());
+                hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+                    if (!error) {
+                        // User successfully stored in MySQL
+                        // Now store the user in sqlite
+                        String uid = jObj.getString("uid");
+
+                        JSONObject user = jObj.getJSONObject("user");
+                        String name = user.getString("name");
+                        String email = user.getString("email");
+                        String created_at = user
+                                .getString("created_at");
+
+                        // Inserting row in users table
+                        db.addUser(name, email, uid, created_at);
+
+                        Toast.makeText(getApplicationContext(), "User successfully registered. Try login now!", Toast.LENGTH_LONG).show();
+
+                        // Launch login activity
+                        Intent intent = new Intent(
+                                RegisterActivity.this,
+                                LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+
+                        // Error occurred in registration. Get the error
+                        // message
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Registration Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting params to register url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("name", name);
+                params.put("email", email);
+                params.put("password", password);
+                params.put("dateOfBirth", dateOfBirth);
+                params.put("gender", gender);
+                params.put("district", district);
+                params.put("guardiansNic", guardiansNic);
+                params.put("nic", nic);
+
+                return params;
+            }
+
+        };
+
+        //Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
     }
 
 
